2f5260c5e5a8637363c2e0e3af8ce3c484d74942
ss3387 <62298338+ss3387@users.noreply.github.com>
2022-12-31 18:22:21 +0100

    Add files via upload

---
+++ server/data.py
@@ -0,0 +1,71 @@
+words = {
+    'Science': [
+        "oxygen", "nitrogen", "pottasium", "silicon", "neutron", "electricity",
+        "energy", "plant cell", "bacteria", "electrolyte", "electron", "distillation",
+        "atmospheric pressure", "fungi", "corona-virus", "vaccine", "antibiotic",
+        "cobalt", "nickel", "engineering", "chemistry", "physics", "manganese",
+        "transition metals", "alkaline earth metals", "standard unit", "displacement"
+        "velocity", "momentum", "photosynthesis", "chlorophyll", "source of energy"
+    ],
+    'Science': [
+        "newton's law of motion", "copper", "conservation of energy", "nuclear fusion",
+        "lithium ion battery", "positive ion", "hydrogen", "helium", "nucleus", "decomposition",
+        "combustion", "periodic table", "electrolysis", "taxonomy", "noble gases", "halogens",
+        "transition metals", "gamma waves", "ultraviolet waves", "speed of light",
+        "dichotomus key", "krypton", "aluminium", "thermal physics", "radioactivity",
+        "density", "stoichiometry", "avogadro's constant", "wavelength", "frequency"
+    ],
+    'History': [
+        "the great war", "christ era", "cold war", "revolution", "middle ages",
+        "joseph stalin", "vladimir lenin", "arab spring", "adolf hitler", "alexander the great",
+        "roman empire", "conflict", "treaty", "causation", "austro-hungarian empire",
+        "democracy", "partition", "monarchy", "dictatorship", "capitalism", "communism"
+    ],
+    'Places': [
+        "berlin wall", "cologne cathedral", "statue of liberty", "himalayas", "mount everest",
+        "red fort", "black forest", "eiffel tower", "taj mahal", "colosseum", "great wall of china",
+        "rhine falls", "niagara falls", "palace of versallies", "grand canyon", "mount fuji",
+        "amsterdam", "antarctica", "baltic sea", "alps mountains", "venice", "bering strait"
+    ],
+    'Food and Drinks': [
+        "french fries", "pizza", "noodles", "burger", "cheese cake", "spaghetti", "croissant",
+        "coca-cola", "orange juice", "flavoured yogurt", "butter brezel", "curry-wurst",
+        "milk chocolate", "almond milk", "cashew nuts", "green tea", "coffee", "wine",
+        "boiled potatoes", "potato chips", "ice cream", "wall nuts", "vegetable salad"
+    ],
+    'Sports': [
+        "european football", "basketball", "ice hockey", "badminton", "cricket", "racqet", 
+        "skiing", "goal post", "hockey stick", "baseball bat", "wheelchair tennis", "shuttle cock",
+        "badminton net", "shoes", "biking", "american football", "fifa tournament",
+        "swimming", "weight lifting", "ice skating", "olympics", "tennis ball"
+    ],
+    'Transportation': [
+        "railways", "waterways", "aircraft", "submarine", "fighter jet", "cargo tram",
+        "passengers", "distance", "fastest route", "maglev technology", "internal combustion engine",
+        "pilot", "bus-network", "racecar", "wheel", "steam engine", "wagon", "boxcar",
+        "infrastructure", "highways", "roadways", "ferry", "ship", "helicopter"
+    ],
+    'Music': [
+        "piano", "flute", "violin", "triangle", "sleigh bells", "guitar", "drum set",
+        "timpani", "alto saxophone", "clarinet", "keyboard", "piccolo", "choir", "jazz",
+        "orchestra", "ensemble", "cello", "double bass", "string instrument", "basoon",
+        "euphonium", "french horn", "trumpet", "baritone", "oboe", "rock band", "waltz"
+    ],
+    'Famous People': [
+        "ludwig van beethoven", "albert einstein", "isaac newton", "mike tyson", "nelson mandela",
+        "charles darwin", "william shakesphere", "abraham lincoln", "mahatma gandhi", "pablo picasso",
+        "martin luther king", "bill gates", "elon musk", "muhammad ali", "christopher columbus",
+        "marie curie", "johann sebastian bach", "vincent van gogh", "leonardo da vinci"
+    ],
+    'Mathematics': [
+        "quadrilateral", "transversal", "cosine rule", "pythagoras theorem", "pentagon", "perimeter",
+        "parabola", "hyperbola", "ellipse graph", "square root", "modulus or remainder",
+        "remainder theorem", "alternate angles", "pi r squared", "half base height", "reflex angle",
+        "vertical stretch", "horizontal shift", "statistics and probability", "straight angle",
+        "compass", "ruler", "centimetres", "metric system", "simultaneous equations"
+    ],
+    'Cars': [
+        "lamborghini sian roadster", "porsche taycan", "tesla model s", "aston martin valkyrie",
+        "ford mustang", "toyota innova", "honda civic", "hyundai accent", ""
+    ],
+}
